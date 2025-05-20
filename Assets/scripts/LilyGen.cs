da72feb45ae0de998d8933910f1d7b3ab90befb7
aryafire1 <erthorsen876@gmail.com>
2025-05-20 15:52:34 -0400

    pond generation practice

--- Assets/scripts/LilyGen.cs
+++ Assets/scripts/LilyGen.cs
@@ -9,32 +9,61 @@ public class LilyGen : MonoBehaviour
     public List<Transform> nodes = new List<Transform>();
 
 
-    void Awake() {
+    void Awake()
+    {
         CreateLists();
         SetPosition();
     }
 
-    void CreateLists() {
-        for (int i=0; i < nodeParent.childCount; ++i) {
+    void CreateLists()
+    {
+        for (int i = 0; i < nodeParent.childCount; ++i)
+        {
             nodes.Add(nodeParent.GetChild(i));
         }
-        for (int i=0; i < lilyParent.childCount; ++i) {
+        for (int i = 0; i < lilyParent.childCount; ++i)
+        {
             lilyPool.Add(lilyParent.GetChild(i));
         }
     }
 
-    void SetPosition() {
-        for (int i=0; i < lilyPool.Count; ++i) {
+    void SetPosition()
+    {
+        for (int i = 0; i < lilyPool.Count; ++i)
+        {
             Transform l = lilyPool[i];
             Transform n = nodes[Random.Range(0, nodes.Count)];
-            l.position = new Vector3(n.position.x, 0, n.position.y) + (RandomSize() * Random.Range(1, 3));
+            l.position = new Vector3(n.position.x, l.position.y, n.position.z) + (new Vector3(1, 0, 1) * Random.Range(1, 3));
             l.localScale = RandomSize();
+            LootProbability(lilyPool[i].gameObject);
         }
     }
 
-    Vector3 RandomSize() {
+    Vector3 RandomSize()
+    {
         float rand = Random.Range(0.25f, 0.75f);
         Vector3 v = new Vector3(rand, 0, rand);
         return v;
+    }
+
+    void LootProbability(GameObject lily)
+    {
+        int rand = Random.Range(1, 100);
+        if (rand <= 10)
+        {
+            lily.GetComponent<Renderer>().material.color = Color.green;
+        }
+        else if (rand >= 25 && rand <= 30)
+        {
+            lily.GetComponent<Renderer>().material.color = Color.yellow;
+        }
+        else if (rand == 40)
+        {
+            lily.GetComponent<Renderer>().material.color = Color.red;
+        }
+        else
+        {
+            return;
+        }
     }
 }
--- Assets/scripts/LilyOverlap.cs
+++ Assets/scripts/LilyOverlap.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class LilyOverlap : MonoBehaviour
 {
     void OnTriggerStay(Collider other) {
-        if (other.gameObject.CompareTag("floor")) {
+        if (other.gameObject.CompareTag("lily")) {
             Debug.Log("collision");
             Vector3 pos = other.transform.position;
             other.transform.position = new Vector3(pos.x + Random.Range(-3f, 3f), pos.y, pos.z + Random.Range(-3f, 3f));
